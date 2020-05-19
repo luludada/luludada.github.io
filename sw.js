0f99a90ccc343e710eeecfab259c5f5177b9bfc5
Mingyang Ye <mingyangye@MingyangdeMacBook-Pro.local>
2020-05-19 16:03:31 +0800

    Change to Horse.svg but it wouldn't work

--- sw.js
+++ sw.js
@@ -1,22 +1,36 @@
-self.addEventListener('install', event => {
-  console.log('V1 installing…');
+const expectedCaches = ['static-v2'];
 
-  // cache a cat SVG
+self.addEventListener('install', event => {
+  console.log('V2 installing…');
+
+  // cache a horse SVG into a new cache, static-v2
   event.waitUntil(
-    caches.open('static-v1').then(cache => cache.add('/cat.svg'))
+    caches.open('static-v2').then(cache => cache.add('/horse.svg'))
   );
 });
 
 self.addEventListener('activate', event => {
-  console.log('V1 now ready to handle fetches!');
+  // delete any caches that aren't in expectedCaches
+  // which will get rid of static-v1
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.map(key => {
+        if (!expectedCaches.includes(key)) {
+          return caches.delete(key);
+        }
+      })
+    )).then(() => {
+      console.log('V2 now ready to handle fetches!');
+    })
+  );
 });
 
 self.addEventListener('fetch', event => {
   const url = new URL(event.request.url);
 
-  // serve the cat SVG from the cache if the request is
+  // serve the horse SVG from the cache if the request is
   // same-origin and the path is '/dog.svg'
   if (url.origin == location.origin && url.pathname == '/dog.svg') {
-    event.respondWith(caches.match('/cat.svg'));
+    event.respondWith(caches.match('/horse.svg'));
   }
 });
