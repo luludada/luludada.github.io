9952534d32d185801fe4482da0c7f9d063446a11
Mingyang Ye <mingyangye@MingyangdeMacBook-Pro.local>
2020-05-21 15:16:41 +0800

    Test a cache strategy with Workbox

--- sw.js
+++ sw.js
@@ -1,36 +1,27 @@
-const expectedCaches = ['static-v2'];
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
-self.addEventListener('install', event => {
-  console.log('V2 installing…');
-
-  // cache a horse SVG into a new cache, static-v2
-  event.waitUntil(
-    caches.open('static-v2').then(cache => cache.add('/horse.svg'))
-  );
-});
-
-self.addEventListener('activate', event => {
-  // delete any caches that aren't in expectedCaches
-  // which will get rid of static-v1
-  event.waitUntil(
-    caches.keys().then(keys => Promise.all(
-      keys.map(key => {
-        if (!expectedCaches.includes(key)) {
-          return caches.delete(key);
-        }
-      })
-    )).then(() => {
-      console.log('V2 now ready to handle fetches!');
+workbox.routing.registerRoute(
+    /\.(?:css|js)$/,
+    new workbox.strategies.StaleWhileRevalidate({
+        "cacheName": "assets",
+        plugins: [
+            new workbox.expiration.Plugin({
+                maxEntries: 1000,
+                maxAgeSeconds: 31536000
+            })
+        ]
     })
-  );
-});
+);
 
-self.addEventListener('fetch', event => {
-  const url = new URL(event.request.url);
-
-  // serve the horse SVG from the cache if the request is
-  // same-origin and the path is '/dog.svg'
-  if (url.origin == location.origin && url.pathname == '/dog.svg') {
-    event.respondWith(caches.match('/horse.svg'));
-  }
-});
+workbox.routing.registerRoute(
+    /\.(?:png|jpg|jpeg|gif|bmp|webp|svg|ico)$/,
+    new workbox.strategies.CacheFirst({
+        "cacheName": "images",
+        plugins: [
+            new workbox.expiration.Plugin({
+                maxEntries: 1000,
+                maxAgeSeconds: 31536000
+            })
+        ]
+    })
+);
