c861289d5e9fc4b1d7d9ccfeba4345c5af54f8d5
Mingyang Ye <mingyangye@MingyangdeMacBook-Pro.local>
2020-05-21 17:23:39 +0800

    Test With Mainfest.json

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,55 @@
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
+const CACHE_NAME = 'cache-v1'
+const DATA_CACHE_NAME = 'data-cache-v1'
 
-if (workbox) {
-  console.log(`Yay! Workbox is loaded 🎉`);
-} else {
-  console.log(`Boo! Workbox didn't load 😬`);
-}
+const PRE_CACHE = ['/index.html', '/css/app.css', '/js/app.js']
 
-console.log('Hello from service-worker.js');
+self.addEventListener('install', e => {
+  console.log('[ServiceWorker] Install')
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(PRE_CACHE)
+    })
+  )
+})
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(response => {
+      if (response) {
+        return response
+      }
+
+      const fetchRequest = e.request.clone()
+
+      return fetch(fetchRequest).then(response => {
+        // Check if we received a valid response
+        if (!response || response.status !== 200) {
+          return response
+        }
+
+        const responseToCache = response.clone()
+
+        caches.open(DATA_CACHE_NAME).then(cache => {
+          cache.put(e.request, responseToCache)
+        })
+
+        return response
+      })
+    })
+  )
+})
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keyList => {
+      return Promise.all(
+        keyList.map(key => {
+          if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+            console.log('[ServiceWorker] Removing old cache', key)
+            return caches.delete(key)
+          }
+        })
+      )
+    })
+  )
+})
