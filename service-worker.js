4b00ebba43df436796a53b0f79d6deb8227d20c6
luludada <mingyangye801@gmail.com>
2020-05-22 14:23:15 +0800

    Set up with offline

--- service-worker.js
+++ service-worker.js
@@ -1,44 +1,19 @@
 const CACHE_NAME = 'cache-v1'
 const DATA_CACHE_NAME = 'data-cache-v1'
 
-const PRE_CACHE = ['/index.html', '/css/app.css', '/js/app.js']
+const PRE_CACHE = ['/index.html', '/styles.css', '/script.js', 'images/icons.png']
 
+
+//On install - as a dependency
 self.addEventListener('install', e => {
-  console.log('[ServiceWorker] Install')
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(PRE_CACHE)
-    })
-  )
-})
+  e.waitUntil(async function() {
+    console.log('[ServiceWorker] Install')
+    const cache = await caches.open(CACHE_NAME);
+    await cache.addAll(PRE_CACHE);
+  }());
+});
 
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(response => {
-      if (response) {
-        return response
-      }
-
-      const fetchRequest = e.request.clone()
-
-      return fetch(fetchRequest).then(response => {
-        // Check if we received a valid response
-        if (!response || response.status !== 200) {
-          return response
-        }
-
-        const responseToCache = response.clone()
-
-        caches.open(DATA_CACHE_NAME).then(cache => {
-          cache.put(e.request, responseToCache)
-        })
-
-        return response
-      })
-    })
-  )
-})
-
+//Clean-up & migration.
 self.addEventListener('activate', e => {
   e.waitUntil(
     caches.keys().then(keyList => {
@@ -53,3 +28,68 @@ self.addEventListener('activate', e => {
     })
   )
 })
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(async function() {
+    const cache = await caches.open('mysite-dynamic');
+    const cachedResponse = await cache.match(event.request);
+    if (cachedResponse) return cachedResponse;
+    const networkResponse = await fetch(event.request);
+    event.waitUntil(
+      cache.put(event.request, networkResponse.clone())
+    );
+    return networkResponse;
+  }());
+});
+
+
+
+
+// self.addEventListener('fetch', (event) => {
+//   event.respondWith(async function() {
+//     const cache = await caches.open('mysite-dynamic');
+//     const cachedResponse = await cache.match(event.request);
+//     const networkResponsePromise = fetch(event.request);
+
+//     event.waitUntil(async function() {
+//       const networkResponse = await networkResponsePromise;
+//       await cache.put(event.request, networkResponse.clone());
+//     }());
+
+//     // Returned the cached response if we have one, otherwise return the network response.
+//     return cachedResponse || networkResponsePromise;
+//   }());
+// });
+
+
+
+
+
+
+// self.addEventListener('fetch', e => {
+//   e.respondWith(
+//     caches.match(e.request).then(response => {
+//       if (response) {
+//         return response
+//       }
+
+//       const fetchRequest = e.request.clone()
+
+//       return fetch(fetchRequest).then(response => {
+//         // Check if we received a valid response
+//         if (!response || response.status !== 200) {
+//           return response
+//         }
+
+//         const responseToCache = response.clone()
+
+//         caches.open(DATA_CACHE_NAME).then(cache => {
+//           cache.put(e.request, responseToCache)
+//         })
+
+//         return response
+//       })
+//     })
+//   )
+// })
+
