ab6cdb26f75fab9873c1b9727250133ebe72779a
luludada <mingyangye801@gmail.com>
2020-05-22 15:35:20 +0800

    Test with only install

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
-const CACHE_NAME = 'cache-v1'
-const DATA_CACHE_NAME = 'data-cache-v1'
+var CACHE_NAME = 'cache-v1'
+var DATA_CACHE_NAME = 'data-cache-v1'
 
-const PRE_CACHE = ['/index.html', '/styles.css', '/script.js', 'images/icons.png'];
+var PRE_CACHE = ['/index.html', '/styles.css', '/script.js', 'images/icons.png'];
 
 //On install - as a dependency
 self.addEventListener('install', e => {
@@ -17,33 +17,33 @@ self.addEventListener('install', e => {
 
 
 //Clean-up & migration.
-self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys().then(keyList => {
-      return Promise.all(
-        keyList.map(key => {
-          if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
-            console.log('[ServiceWorker] Removing old cache', key);
-            return caches.delete(key)
-          }
-        })
-      )
-    })
-  )
-})
+// self.addEventListener('activate', e => {
+//   e.waitUntil(
+//     caches.keys().then(keyList => {
+//       return Promise.all(
+//         keyList.map(key => {
+//           if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+//             console.log('[ServiceWorker] Removing old cache', key);
+//             return caches.delete(key)
+//           }
+//         })
+//       )
+//     })
+//   )
+// })
 
-self.addEventListener('fetch', (event) => {
-  event.respondWith(async function() {
-    const cache = await caches.open('mysite-dynamic');
-    const cachedResponse = await cache.match(event.request);
-    if (cachedResponse) return cachedResponse;
-    const networkResponse = await fetch(event.request);
-    event.waitUntil(
-      cache.put(event.request, networkResponse.clone())
-    );
-    return networkResponse;
-  }());
-});
+// self.addEventListener('fetch', (event) => {
+//   event.respondWith(async function() {
+//     const cache = await caches.open('mysite-dynamic');
+//     const cachedResponse = await cache.match(event.request);
+//     if (cachedResponse) return cachedResponse;
+//     const networkResponse = await fetch(event.request);
+//     event.waitUntil(
+//       cache.put(event.request, networkResponse.clone())
+//     );
+//     return networkResponse;
+//   }());
+// });
 
 
 
